1d7f70877e199421618b5173b939377e5a422118
Gia Hau <nguyengiahau2004@gmail.com>
2024-04-27 19:45:54 +0700

    Lesson 2

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init route
+app.get("/", (req, res, next) => {
+    const strCompress = "Hello Gia Hau";
+    return res.status(200).json({
+        message: "Wellcome Gia Hau",
+        metadata: strCompress.repeat(10000)
+    });
+});
+
+// handing error
+
+module.exports = app;
